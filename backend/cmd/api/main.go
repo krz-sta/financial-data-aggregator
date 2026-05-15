f55768a5dbf8d1832a3782e28c602eb205cd3d5d
r4qq <r4qq@protonmail.com>
2026-05-15 21:23:56 +0200

    create test db connection and endpoint

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"financial-data-aggregator-backend/internal/models"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	router := gin.Default()
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Nie udało połączyć sie z bazą")
+	}
+
+	fmt.Println("Połączono z bazą")
+
+	db.AutoMigrate(&models.User{}, &models.PortfolioItem{})
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
