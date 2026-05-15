a5557a18bfdfc24d76d5e1138f57301f2124ee41
r4qq <r4qq@protonmail.com>
2026-05-15 21:23:29 +0200

    fix gorm types

--- backend/internal/models/user_model.go
+++ backend/internal/models/user_model.go
@@ -11,14 +11,14 @@ type PortfolioItem struct {
 	UserID    uuid.UUID `gorm:"type:uuid;not null;index" json:"userID"`
 	Symbol    string    `gorm:"type:varchar(20);not null;index" json:"symbol"`
 	Amount    float64   `gorm:"type:numeric;not null;" json:"amount"`
-	CreatedAt time.Time `gorm:"type:autoCreatetime" json:"craetedAt"`
-	UpdatedAt time.Time `gorm:"type:autoUpdatetime" json:"UpdatedAt"`
+	CreatedAt time.Time `gorm:"type:timestamp" json:"craetedAt"`
+	UpdatedAt time.Time `gorm:"type:timestamp" json:"UpdatedAt"`
 }
 
 type User struct {
 	ID           uuid.UUID       `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
 	Email        string          `gorm:"type:varchar(255);not null" json:"email"`
 	PasswordHash string          `gorm:"type:varchar(255);not null" json:"-"`
-	CreatedAt    time.Time       `gorm:"type:autoCreatetime" json:"craetedAt"`
+	CreatedAt    time.Time       `gorm:"type:timestamp" json:"craetedAt"`
 	Portfolio    []PortfolioItem `gorm:"foreignKey:UserID" json:"portfolio,omitempty"`
 }
