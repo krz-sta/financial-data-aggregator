5fd421ed060e645c5e3a1e13a9f7b18f5a13a852
r4qq <r4qq@protonmail.com>
2026-05-18 00:09:26 +0200

    add config.go to clean main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"financial-data-aggregator-backend/internal/config"
 	"financial-data-aggregator-backend/internal/models"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -12,25 +13,26 @@ import (
 )
 
 func main() {
+	var db *gorm.DB
+	var err error
+
 	router := gin.Default()
 
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+	cfg := config.LoadConfig()
+	dsn := cfg.DB.GetDsn()
+	rAddr := cfg.Router.GetRouterConfig()
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Nie udało połączyć sie z bazą")
+		log.Fatalf("Cannot connect to DB: %v", err.Error())
 	}
 
-	fmt.Println("Połączono z bazą")
+	fmt.Println("Connected to DB")
 
-	db.AutoMigrate(&models.User{}, &models.PortfolioItem{})
+	err = db.AutoMigrate(&models.User{}, &models.PortfolioItem{})
+	if err != nil {
+		log.Fatalf("Couldn't automigrate: %v", err.Error())
+	}
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -38,5 +40,5 @@ func main() {
 		})
 	})
 
-	router.Run("0.0.0.0:8080")
+	router.Run(rAddr)
 }
---
+++ backend/internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DB     *DBconfig
+	Router *RouterConfig
+}
+
+type DBconfig struct {
+	Host     string
+	User     string
+	Password string
+	Name     string
+	Port     string
+}
+
+type RouterConfig struct {
+	Host string
+	Port string
+}
+
+func (db *DBconfig) GetDsn() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		db.Host, db.User, db.Password, db.Name, db.Port)
+}
+
+func (r *RouterConfig) GetRouterConfig() string {
+	return fmt.Sprintf("%s:%s", r.Host, r.Port)
+}
+
+func LoadConfig() *Config {
+	dbCfg := &DBconfig{
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		Port:     os.Getenv("DB_PORT"),
+	}
+
+	routerCfg := &RouterConfig{
+		Host: os.Getenv("ROUTER_HOST"),
+		Port: os.Getenv("ROUTER_PORT"),
+	}
+
+	return &Config{
+		DB:     dbCfg,
+		Router: routerCfg,
+	}
+}
