7321ae2f1ee2949a0acb04ea7680427f3e646a6e
r4qq <r4qq@protonmail.com>
2026-05-24 00:22:03 +0200

    finish middleware

--- backend/internal/middleware/auth_middleware.go
+++ backend/internal/middleware/auth_middleware.go
@@ -1,10 +1,56 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 func AuthMiddleware(secretKey string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader = c.GetHeader("Authorization")
+		authHeader := c.GetHeader("Authorization")
 
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Auth header is missing"})
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Wrong token format"})
+			return
+		}
+
+		tokenString := parts[1]
+
+		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unknown signing method: %v", t.Header["alg"])
+			}
+			return []byte(secretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token"})
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token"})
+			return
+		}
+
+		if userID, ok := claims["sub"].(string); ok {
+			c.Set("userID", userID)
+			c.Next()
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token"})
+			return
+		}
 	}
 }
