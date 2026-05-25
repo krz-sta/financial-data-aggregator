b47a912dc379e20a57aff60aebd907a8a480ec8c
r4qq <r4qq@protonmail.com>
2026-05-25 21:51:51 +0200

    add health check for db, some minor spelling fixes, TODO: check for redis when added

---
+++ backend/internal/handlers/health/health.go
@@ -0,0 +1,35 @@
+package health
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	return &Handler{
+		DB: db,
+	}
+}
+
+func (h Handler) DBHealth(ctx *gin.Context) {
+	db, err := h.DB.DB()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "DB is down"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
+	return
+}
