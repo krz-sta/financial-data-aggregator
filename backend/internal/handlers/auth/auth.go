973f3d9ac09222e38c2866b64375e625a67f8581
r4qq <r4qq@protonmail.com>
2026-06-01 13:05:12 +0200

    update auth handler

--- backend/internal/handlers/auth/auth.go
+++ backend/internal/handlers/auth/auth.go
@@ -1,16 +1,11 @@
 package auth
 
 import (
-	"errors"
-	"financial-data-aggregator-backend/internal/models"
+	"financial-data-aggregator-backend/internal/service"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
 
 type registerInput struct {
@@ -31,14 +26,12 @@ type userResponse struct {
 }
 
 type Handler struct {
-	DB     *gorm.DB
-	JWTKey string
+	authService service.AuthService
 }
 
-func NewHandler(db *gorm.DB, jwtKey string) *Handler {
+func NewHandler(authService service.AuthService) *Handler {
 	return &Handler{
-		DB:     db,
-		JWTKey: jwtKey,
+		authService: authService,
 	}
 }
 
@@ -50,42 +43,13 @@ func (h *Handler) Register(ctx *gin.Context) {
 		return
 	}
 
-	var count int64
-	err := h.DB.Model(&models.User{}).Where("email = ?", input.Email).Count(&count).Error
+	user, err := h.authService.Register(input.Email, input.Name, input.Password)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	if count > 0 {
-		ctx.JSON(http.StatusConflict, gin.H{"error": "email already registered"})
-		return
-	}
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	newUser := models.User{
-		ID:           uuid.New(),
-		Email:        input.Email,
-		DisplayName:  input.Name,
-		PasswordHash: string(passwordHash),
-	}
-
-	err = h.DB.Create(&newUser).Error
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	ctx.JSON(http.StatusCreated, gin.H{"data": userResponse{
-		ID:          newUser.ID,
-		Email:       newUser.Email,
-		DisplayName: newUser.DisplayName,
-	}})
+	ctx.JSON(http.StatusCreated, gin.H{"data": user})
 }
 
 func (h *Handler) Login(ctx *gin.Context) {
@@ -96,36 +60,9 @@ func (h *Handler) Login(ctx *gin.Context) {
 		return
 	}
 
-	var tempUsr models.User
-	err := h.DB.Model(&models.User{}).Where("email = ?", input.Email).First(&tempUsr).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(tempUsr.PasswordHash), []byte(input.Password))
+	tokenString, err := h.authService.Login(input.Email, input.Password)
 	if err != nil {
 		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-		return
-	}
-
-	// sub (Subject) - ID użytkownika
-	// exp (Expiration Time) - czas wygaśnięcia tokenu w formacie Unix
-	// iat (Issued at) - czas wydania tokenu w formacie Unix
-	claims := jwt.MapClaims{
-		"sub": tempUsr.ID.String(),
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-		"iat": time.Now().Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString([]byte(h.JWTKey))
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate token"})
 		return
 	}
 
