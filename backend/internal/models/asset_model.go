a2319f1705bcc5c5f12fc17c912a3a54b4745e58
r4qq <r4qq@protonmail.com>
2026-06-13 00:45:03 +0200

    models for asset and specific type for response from frankfurter

--- backend/internal/models/asset_model.go
+++ backend/internal/models/asset_model.go
@@ -6,3 +6,9 @@ type AssetInfo struct {
 	Type   string `json:"type"`
 	ApiID  string `json:"-"` //coingecko
 }
+
+type FrankfurterAsset struct {
+	Base  string  `json:"base"`
+	Quote string  `json:"quote"`
+	Rate  float64 `json:"rate"`
+}
