4ee3a307b66bbaaf16a2eba1ffbead0e28a72efa
r4qq <r4qq@duck.com>
2026-06-13 16:53:49 +0200

    add healtchekc fo redis

--- backend/internal/handlers/health/health.go
+++ backend/internal/handlers/health/health.go
@@ -4,16 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
 type Handler struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	Redis *redis.Client
 }
 
-func NewHandler(db *gorm.DB) *Handler {
+func NewHandler(db *gorm.DB, redis *redis.Client) *Handler {
 	return &Handler{
-		DB: db,
+		DB:    db,
+		Redis: redis,
 	}
 }
 
@@ -27,6 +30,15 @@ func (h Handler) DBHealth(ctx *gin.Context) {
 	err = db.Ping()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "DB is down"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
+}
+
+func (h Handler) RedisHealth(ctx *gin.Context) {
+	if err := h.Redis.Ping(ctx.Request.Context()).Err(); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Redis is down"})
 		return
 	}
 
